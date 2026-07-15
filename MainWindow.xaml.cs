3676f428e6ee08d1f1f41e176f3c765529419c86
R00T-Amir <aj329056@gmail.com>
2026-07-15 17:56:56 +0330

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,25 +11,18 @@ namespace ChronoDesk
     public partial class MainWindow : Window
     {
         private readonly ITimeEngine _timeEngine;
-        private readonly ISettingsEngine _settingsEngine;
+        private readonly WidgetConfig _config;
 
-        public MainWindow(ITimeEngine timeEngine, ISettingsEngine settingsEngine)
+        public MainWindow(ITimeEngine timeEngine, WidgetConfig config)
         {
             InitializeComponent();
             _timeEngine = timeEngine;
-            _settingsEngine = settingsEngine;
+            _config = config;
 
-            if (_settingsEngine.Settings.Left == 100 && _settingsEngine.Settings.Top == 100)
-            {
-                this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            }
-            else
-            {
-                this.Left = _settingsEngine.Settings.Left;
-                this.Top = _settingsEngine.Settings.Top;
-            }
-
-            // اعمال تنظیمات ذخیره شده در استارتاپ
+            this.Left = _config.Left;
+            this.Top = _config.Top;
+            
+            CityText.Text = _config.CityName;
             ApplyAppearance();
 
             var timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
@@ -41,17 +34,27 @@ namespace ChronoDesk
 
         private void ApplyAppearance()
         {
-            this.Opacity = _settingsEngine.Settings.Opacity;
-            MainBorder.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_settingsEngine.Settings.BackgroundColor));
-            TimeText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_settingsEngine.Settings.TextColor));
-            DateText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_settingsEngine.Settings.DateColor));
+            this.Opacity = _config.Opacity;
+            MainBorder.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_config.BackgroundColor));
+            TimeText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_config.TextColor));
+            DateText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_config.DateColor));
+            CityText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_config.DateColor));
         }
 
         private void UpdateClock(object? sender, EventArgs e)
         {
-            var time = _timeEngine.GetCurrentTime().ToLocalTime();
-            TimeText.Text = time.ToString("HH:mm:ss");
-            DateText.Text = time.ToString("dddd, dd MMMM yyyy");
+            try
+            {
+                var tz = TimeZoneInfo.FindSystemTimeZoneById(_config.TimeZoneId);
+                var time = TimeZoneInfo.ConvertTimeFromUtc(_timeEngine.GetCurrentTime(), tz);
+                TimeText.Text = time.ToString("HH:mm:ss");
+                DateText.Text = time.ToString("dddd, dd MMMM yyyy");
+            }
+            catch
+            {
+                var time = _timeEngine.GetCurrentTime().ToLocalTime();
+                TimeText.Text = time.ToString("HH:mm:ss");
+            }
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -59,37 +62,34 @@ namespace ChronoDesk
             if (e.ButtonState == MouseButtonState.Pressed) DragMove();
         }
 
-        private void Window_Closing(object? sender, CancelEventArgs e)
+        protected override void OnClosing(CancelEventArgs e)
         {
-            _settingsEngine.Settings.Left = this.Left;
-            _settingsEngine.Settings.Top = this.Top;
-            _settingsEngine.Save();
+            // ذخیره موقعیت این ویجت خاص هنگام بسته شدن
+            _config.Left = this.Left;
+            _config.Top = this.Top;
+            base.OnClosing(e);
         }
 
-        // --- منطق تغییر تم ---
         private void Theme_Dark_Click(object sender, RoutedEventArgs e) => UpdateTheme("#CC1F1F1F", "#FFFFFF", "#99FFFFFF");
         private void Theme_Light_Click(object sender, RoutedEventArgs e) => UpdateTheme("#CCF0F0F0", "#1F1F1F", "#991F1F1F");
         private void Theme_Cyber_Click(object sender, RoutedEventArgs e) => UpdateTheme("#CC001E3C", "#00FFFF", "#9900FFFF");
 
         private void UpdateTheme(string bg, string text, string date)
         {
-            _settingsEngine.Settings.BackgroundColor = bg;
-            _settingsEngine.Settings.TextColor = text;
-            _settingsEngine.Settings.DateColor = date;
+            _config.BackgroundColor = bg;
+            _config.TextColor = text;
+            _config.DateColor = date;
             ApplyAppearance();
-            _settingsEngine.Save();
         }
 
-        // --- منطق تغییر شفافیت ---
         private void Opacity_100_Click(object sender, RoutedEventArgs e) => UpdateOpacity(1.0);
         private void Opacity_80_Click(object sender, RoutedEventArgs e) => UpdateOpacity(0.8);
         private void Opacity_50_Click(object sender, RoutedEventArgs e) => UpdateOpacity(0.5);
 
         private void UpdateOpacity(double opacity)
         {
-            _settingsEngine.Settings.Opacity = opacity;
+            _config.Opacity = opacity;
             this.Opacity = opacity;
-            _settingsEngine.Save();
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
