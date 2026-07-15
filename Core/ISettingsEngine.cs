978e398f518e1bbe5b354a4929798841443973df
R00T-Amir <aj329056@gmail.com>
2026-07-15 17:41:46 +0330

    Update ISettingsEngine.cs

--- Core/ISettingsEngine.cs
+++ Core/ISettingsEngine.cs
@@ -11,5 +11,11 @@ namespace ChronoDesk.Core
     {
         public double Left { get; set; } = 100;
         public double Top { get; set; } = 100;
+        
+        // تنظیمات شخصی‌سازی
+        public string BackgroundColor { get; set; } = "#CC1F1F1F";
+        public string TextColor { get; set; } = "#FFFFFF";
+        public string DateColor { get; set; } = "#99FFFFFF";
+        public double Opacity { get; set; } = 1.0;
     }
 }
